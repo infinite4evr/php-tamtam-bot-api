e89342088f95770d917759f0a461fe44b631b90f
Ashu <ggs.sudhanshu@gmail.com>
2019-09-27 01:07:27 +0530

    Updated script details

--- TamTam.php
+++ TamTam.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 /**
- * TamTam Error Logger Class *
+ * TamTam Main Class *
  * @author Ashu (github.com/infinite4evr) <ggs.sudhanshu@gmail.com>
  */
 
