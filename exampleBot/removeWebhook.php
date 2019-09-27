3b0a04e28852f64bb6f1d3459c2e9802b423e6e3
Ashu <ggs.sudhanshu@gmail.com>
2019-09-27 14:58:10 +0530

    Update removeWebhook.php

--- exampleBot/removeWebhook.php
+++ exampleBot/removeWebhook.php
@@ -3,8 +3,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once(dirname(__FILE__).'/../TamTam.php');
-$bot = new TamTam('ZpW8TbSL3d_kU-Yh50LQn-45zrqY0JiiCPWnODVD1KY');
-$bot->deleteWebhook('https://api.freeroid.com/bots/tamExample/exampleBot/example.php');
+$bot = new TamTam('token');
+$bot->deleteWebhook('https://api.freeroid.com/');
 
 
 
