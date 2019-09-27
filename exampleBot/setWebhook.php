761fb3c36a6069371090c06445dcc6225cfabf1e
Ashu <ggs.sudhanshu@gmail.com>
2019-09-27 14:48:05 +0530

    Update setWebhook.php

--- exampleBot/setWebhook.php
+++ exampleBot/setWebhook.php
@@ -3,8 +3,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once(dirname(__FILE__).'/../TamTam.php');
-$bot = new TamTam('ZpW8TbSL3d_kU-Yh50LQn-45zrqY0JiiCPWnODVD1KY');
-$bot->setWebhook('https://timezoneret.000webhostapp.com/TamTamExample/exampleBot/exampleBot.php');
+$bot = new TamTam('Bot Token');
+$bot->setWebhook('https://yourwebhook.com');
 
 
 
